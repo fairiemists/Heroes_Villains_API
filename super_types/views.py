bae0684fdb5a91949df355077c28ca7da5bf7e55
fairiemists <v.deweyness@gmail.com>
2022-08-17 13:23:01 -0700

    Added POST function to Views files

--- super_types/views.py
+++ super_types/views.py
@@ -5,11 +5,19 @@ from rest_framework import status
 from .serializers import SuperTypeSerializer
 from .models import SuperType
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def super_types_list(request):
-    super_types = SuperType.objects.all()
-    serializer = SuperTypeSerializer(super_types, many=True)
-    return Response(serializer.data)
+    
+    if request.method == 'GET':
+        super_types = SuperType.objects.all()
+        serializer = SuperTypeSerializer(super_types, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = SuperTypeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
--- supers/views.py
+++ supers/views.py
@@ -5,11 +5,20 @@ from rest_framework import status
 from .serializers import SuperSerializer
 from .models import Super
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
-    supers = Super.objects.all()
-    serializer = SuperSerializer(supers, many=True)
-    return Response(serializer.data)
+    
+    if request.method == 'GET':
+        supers = Super.objects.all()
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
