da757cc515a51491254132948083c312171064d6
fairiemists <v.deweyness@gmail.com>
2022-08-17 12:31:14 -0700

    Created Superuser + Entered Views files for Supers & SuperTypes Apps + Created URL files

--- super_types/views.py
+++ super_types/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SuperTypeSerializer
+from .models import SuperType
 
-# Create your views here.
+@api_view(['GET', 'PUT', 'DELETE'])
+def super_types_list(request, pk):
+    super_type = get_object_or_404(SuperType, pk=pk)
+    if request.method == 'GET':
+        serializer = SuperTypeSerializer(super_type);
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = SuperTypeSerializer(super_type, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        super_type.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SuperSerializer
+from .models import Super
 
-# Create your views here.
+@api_view(['GET', 'PUT', 'DELETE'])
+def supers_list(request, pk):
+    super = get_object_or_404(Super, pk=pk)
+    if request.method == 'GET':
+        serializer = SuperSerializer(super);
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
