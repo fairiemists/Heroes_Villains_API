bebd265deda7f797c8eaacffd0269971a9fb2df8
fairiemists <v.deweyness@gmail.com>
2022-08-17 10:52:19 -0700

    Created Models Supers and SuperTypes + Migrated them

--- super_types/models.py
+++ super_types/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class SuperType(models.Model):
-    type: models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
     
